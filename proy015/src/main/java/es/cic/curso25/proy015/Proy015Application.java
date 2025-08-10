57005c260afcecf3b1b877361fce251fac03e4c9
ricardo <ricardo@pc.com>
2025-08-10 13:31:29 +0200

    primer commit

---
+++ proy015/src/main/java/es/cic/curso25/proy015/Proy015Application.java
@@ -0,0 +1,13 @@
+package es.cic.curso25.proy015;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proy015Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Proy015Application.class, args);
+	}
+
+}
---
+++ proy015/src/test/java/es/cic/curso25/proy015/Proy015ApplicationTests.java
@@ -0,0 +1,13 @@
+package es.cic.curso25.proy015;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Proy015ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
